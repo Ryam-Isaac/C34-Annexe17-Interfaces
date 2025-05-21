650085c7d01deae6f709469b9feb57452e7244db
Ryam_Isaac <ryamisaac1@gmail.com>
2025-05-21 12:42:04 -0400

    Update Proprietaire.java

--- Proprietaire.java
+++ Proprietaire.java
@@ -1,1 +1,26 @@
 
+package projet_interface;
+
+import java.time.LocalDate;
+import java.util.GregorianCalendar;
+
+public class Proprietaire {
+
+    private String villeRes;
+    private LocalDate dateNaissance;
+
+    public Proprietaire(String villeRes, LocalDate dateNaissance)
+    {
+        this.villeRes = villeRes;
+        this.dateNaissance = dateNaissance;
+    }
+
+    public LocalDate getDate ()
+    {
+        return dateNaissance;
+    }
+    public String getVille()
+    {
+        return villeRes;
+    }
+}
